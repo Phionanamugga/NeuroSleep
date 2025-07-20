288da90915b50f6904b876d84a1c5bd392d5b72c
Phiona Namugga <phionanamugga15@gmail.com>
2025-07-20 23:41:06 +0000

    creates model for the users login data

---
+++ app/models/users.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    # One-to-one relationship with UserData
+    user_data = relationship("UserData", back_populates="user", uselist=False)
